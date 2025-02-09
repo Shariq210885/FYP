c63d826d14be7fea119b18add0b0b4dc3d86e19b
Talal Mohiuddin <hafiztalal8722@gmail.com>
2025-02-10 00:29:49 +0500

    Refactor NavBar component to simplify role checks and improve button rendering logic

--- front-end/src/components/dashboardHeader/NavBar.jsx
+++ front-end/src/components/dashboardHeader/NavBar.jsx
@@ -87,10 +87,10 @@ function NavBar({ navbarLinks, isTenant }) {
                 className="flex items-center gap-2 px-2 py-2 text-white rounded-lg bg-primaryColor hover:bg-primaryColor/90"
                 onClick={() => navigate("/admin/")}
               >
-                <span className="flex items-center gap-1">Admin Panel</span>
+                <span>Admin Panel</span>
               </button>
             )}
-            {isTenant && (
+            {isTenant && !user?.role && (
               <button
                 className="flex items-center gap-2 px-2 py-2 text-white rounded-lg bg-primaryColor hover:bg-primaryColor/90"
                 onClick={() => {
@@ -107,19 +107,9 @@ function NavBar({ navbarLinks, isTenant }) {
                   }
                 }}
               >
-                {user?.role === "landowner" ? (
-                  <span className="flex items-center gap-1">
-                    List Your Property <FaHouseChimneyUser size={20} />
-                  </span>
-                ) : user?.role === "serviceman" ? (
-                  <span className="flex items-center gap-1">
-                    List Your Service <FaUserTie size={20} />
-                  </span>
-                ) : user?.role === "admin" ? null : (
-                  <span className="flex items-center gap-1">
-                    List Your Property <FaHouseChimneyUser size={20} />
-                  </span>
-                )}
+                <span className="flex items-center gap-1">
+                  List Your Property <FaHouseChimneyUser size={20} />
+                </span>
               </button>
             )}
             <div className="flex items-center gap-2">
