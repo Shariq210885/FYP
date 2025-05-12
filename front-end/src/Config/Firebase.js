ed824f9f22095cd4839dfe8658fddf350f31f750
Talal Mohiuddin <hafiztalal8722@gmail.com>
2025-05-13 01:37:51 +0500

    Update Firebase configuration with new project details

--- front-end/src/Config/Firebase.js
+++ front-end/src/Config/Firebase.js
@@ -1,19 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBmrl-3uJLKhe33N8J0lSg-4FM_OsE6UW0",
-  authDomain: "makaan-b398e.firebaseapp.com",
-  projectId: "makaan-b398e",
-  storageBucket: "makaan-b398e.firebasestorage.app",
-  messagingSenderId: "92428535510",
-  appId: "1:92428535510:web:b14454b968ea4543182b77",
-  measurementId: "G-FLPK64EZV8"
+  apiKey: "AIzaSyAcXUg4t0dvHphOpA0xSzYw2j7hFP3fk_k",
+  authDomain: "fyp25-ed434.firebaseapp.com",
+  projectId: "fyp25-ed434",
+  storageBucket: "fyp25-ed434.firebasestorage.app",
+  messagingSenderId: "888684828483",
+  appId: "1:888684828483:web:f50d0713e275315e3741f6",
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
