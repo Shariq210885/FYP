ef79de8c8611d44e00c8e737e451c6300dac17d8
Talal Mohiuddin <hafiztalal8722@gmail.com>
2025-05-14 16:28:39 +0500

    Update API instance base URL and enhance request/response interceptors

--- front-end/src/api/instance.js
+++ front-end/src/api/instance.js
@@ -1,31 +1,33 @@
 import axios from "axios";
 
+
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://fyp-3kn1.onrender.com/api/",
 });
 instance.interceptors.request.use(
-  (config) => {
-    // Set headers dynamically based on data type
-    if (config.data instanceof FormData) {
-      config.headers["Content-Type"] = "multipart/form-data";
-    } else {
-      config.headers["Content-Type"] = "application/json";
+    (config) => {
+      // Set headers dynamically based on data type
+      if (config.data instanceof FormData) {
+        config.headers["Content-Type"] = "multipart/form-data";
+      } else {
+        config.headers["Content-Type"] = "application/json";
+      }
+      return config;
+    },
+    (error) => {
+      console.error("Request Error:", error);
+      return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    console.error("Request Error:", error);
-    return Promise.reject(error);
-  }
-);
+  );
+
 
 instance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+    (response) => {
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
 );
 
-export default instance;
+export default instance
